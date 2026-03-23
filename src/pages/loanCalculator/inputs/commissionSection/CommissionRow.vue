a56baca8f957583daae5dfa8a0c031b161ac6cba
Narekkk2000 <narek.sargsyan.2023@gmail.com>
2026-03-23 17:14:12 +0400

    resized

--- src/pages/loanCalculator/inputs/commissionSection/CommissionRow.vue
+++ src/pages/loanCalculator/inputs/commissionSection/CommissionRow.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { onMounted, onUnmounted, ref } from 'vue'
 import { useCommissionRow } from '@/composables/features/loan/useCommissionRow'
 import type { CommissionInput } from '@/types/loan'
 import BaseText from '@/components/base/text/BaseText.vue'
@@ -26,10 +27,15 @@ const {
   () => props.modelValue,
   (val) => emit('update:modelValue', val),
 )
+
+const isSmall = ref(window.innerWidth < 768)
+const onResize = () => { isSmall.value = window.innerWidth < 768 }
+onMounted(() => window.addEventListener('resize', onResize))
+onUnmounted(() => window.removeEventListener('resize', onResize))
 </script>
 
 <template>
-  <BaseFlex gap="3" class="border border-gray-200 p-2 rounded-[6px] relative" style="border-top-left-radius: 0px;">
+  <BaseFlex :col="isSmall" gap="3" class="border border-gray-200 p-2 rounded-[6px] relative" style="border-top-left-radius: 0px;">
     <div class="px-2 py-1 bg-[#F3F8FE] text-black text-xs font-bold font-arm rounded-sm absolute top-[-22px] left-[-1px]" style="border-bottom-left-radius: 0px; border-bottom-right-radius: 0px;">{{ label }}</div>
     <!-- Label -->
     <div class="w-24 shrink-0">
@@ -38,10 +44,11 @@ const {
       </BaseText>
     </div>
 
-    <!-- Percent field -->
+    <div class="flex  items-center gap-3">
+          <!-- Percent field -->
     <div class="flex-1">
       <BaseText tag="p" variant="hint" color="CONTENT" class="mb-5 font-bold">Տոկոս</BaseText>
-      <InputField label="" type="number" inputmode="decimal" :model-value="localPercent" :min="0" :step="0.1"
+      <InputField  label="" type="number" inputmode="decimal" :model-value="localPercent" :min="0" :step="0.1"
         :error="percentError" placeholder="0" has-suffix @update:model-value="onPercentInput">
         <template #suffix>%</template>
       </InputField>
@@ -57,6 +64,7 @@ const {
         :error="amountError" placeholder="0" has-prefix @update:model-value="onAmountInput">
         <template #prefix>$</template>
       </InputField>
+    </div>
     </div>
   </BaseFlex>
 </template>
